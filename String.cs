d3cbc467e15802c4f9e56429bd94f1fa2ed4296d
Rishabh28111995 <109350653+Rishabh28111995@users.noreply.github.com>
2023-05-03 01:37:32 +0530

    Add files via upload

---
+++ String.cs
@@ -0,0 +1,27 @@
+﻿namespace StringCase;
+class MaxString
+{
+    public static T FindMax<T>(T num1, T num2, T num3) where T : IComparable
+    {
+        T max = num1;
+        if (num2.CompareTo(max) > 0)
+        {
+            max = num2;
+        }
+        if (num3.CompareTo(max) > 0)
+        {
+            max = num3;
+        }
+        return max;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        string max1 = MaxString.FindMax("Apple", "Peach", "Banana");
+        Console.WriteLine($"Max. string is : {max1}");
+
+    }
+}
